8237ec2c7d63087c9c6752b830aa38a3e5e40919
Tyler <tylershelton99@gmail.com>
2021-08-08 21:45:20 +0000

    Completed Sum From a to b Recursively

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let result = 0;  // create a variable to store the result
 
+  // as soon as fromN is greater than toN, we end recursion.
+  if (fromN > toN) {
+    return 0;
+  }
+  // add the current value of fromN and run sum again adding 1 to fromN to get closer to toN
+  result = fromN + sum(fromN + 1, toN);
+  return result;  // return the result
+}
 module.exports = sum;
